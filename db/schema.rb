b2205d7c85a7736299694bf39400ea6bd15f55c1
Ian Stanfield <istanfield09@gmail.com>
2017-03-10 17:55:40 -0800

    Create events via scaffolding
    Create events via scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170311013225) do
+ActiveRecord::Schema.define(version: 20170311015327) do
+
+  create_table "events", force: :cascade do |t|
+    t.text     "name"
+    t.date     "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "organizations", force: :cascade do |t|
     t.datetime "created_at", null: false
