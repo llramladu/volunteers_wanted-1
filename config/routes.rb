898598842df78831e389bbffee91f5b710c9b044
istanfie <istanfie@colquitt.dreamhost.com>
2017-03-16 19:59:09 -0700

    Adds skills controller and routes and additional tests

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
 
   resources :users
 
+  resources :skills
+  resources :traits
+  resources :works
+  resources :positions
+  resources :volunteers
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
