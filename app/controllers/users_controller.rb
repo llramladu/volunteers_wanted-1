bc42531ae469a88b904f419002312d24d7add4b1
istanfie <istanfie@colquitt.dreamhost.com>
2017-03-19 17:26:22 -0700

    Adds more robust forms to events, organizations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,6 +71,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :name, :biography, :education, :phoneNum, :birthdate, :availabilityBegin, :availabilityEnd) 
+      params.require(:user).permit(:email, :password, :password_confirmation, :name, :biography, :education, :phoneNum, :birthDate, :availabilityBegin, :availabilityEnd) 
     end
 end
